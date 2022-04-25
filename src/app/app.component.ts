700e7506182f90d495f84557d8c71b9632a8175a
MichaelDaRocha <50926202+MichaelDaRocha@users.noreply.github.com>
2022-04-24 21:54:38 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Michael DaRocha';
+
+  public constructor(private titleService: Title){}
+
+  public ngOnInit(): void {
+    this.titleService.setTitle(this.title);
+  }
+}
